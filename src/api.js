f1a7384385fcb30f4711cafd5dcb42b15cd6df69
JIN <0626na@gmail.com>
2022-04-20 14:58:30 +0900

    api http => https로 변경

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://api.data.go.kr/openapi/tn_pubr_public_toilet_api";
+const BASE_URL = "https://api.data.go.kr/openapi/tn_pubr_public_toilet_api";
 const key = `%2F1wtW%2B7gGkadBPCLIOEHyd8Zl9k7kkypDM1goWeVJ0lx9hBtTc93sMeud2%2FoEeBqgZpxoalUdYBNL0%2FBYxggPQ%3D%3D`;
 const dataType = "json";
 

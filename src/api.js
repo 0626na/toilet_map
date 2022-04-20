dd22aea7a1eadf487f7727c06ce4b6e5087ad2bd
JIN <0626na@gmail.com>
2022-04-20 15:22:34 +0900

    헤로쿠 프록시 서버 사용

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
-const BASE_URL = "https://api.data.go.kr/openapi/tn_pubr_public_toilet_api";
+const BASE_URL = "http://api.data.go.kr/openapi/tn_pubr_public_toilet_api";
 const key = `%2F1wtW%2B7gGkadBPCLIOEHyd8Zl9k7kkypDM1goWeVJ0lx9hBtTc93sMeud2%2FoEeBqgZpxoalUdYBNL0%2FBYxggPQ%3D%3D`;
 const dataType = "json";
 
 export const toiletApi = (pageNumber, rows) => {
-  const URL = `${BASE_URL}?serviceKey=${key}&numOfRows=${rows}&type=${dataType}&pageNo=${pageNumber}`;
+  const URL = `https://reactproxyserver.herokuapp.com/${BASE_URL}?serviceKey=${key}&numOfRows=${rows}&type=${dataType}&pageNo=${pageNumber}`;
 
   return fetch(URL).then((res) => res.json());
 };
